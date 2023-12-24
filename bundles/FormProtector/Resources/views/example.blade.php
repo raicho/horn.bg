7fc68711414bc0fde03a852e0247496a8483fe0f
Райчо <raioosered@gmail.com>
2023-12-24 11:25:29 +0200

    update vite config and change public path

--- bundles/FormProtector/Resources/views/example.blade.php
+++ bundles/FormProtector/Resources/views/example.blade.php
@@ -1,38 +1,29 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Welcome view</title>
-</head>
-<body>
+@extends('layouts.master')
 
-
-<form method="post" action="{{ route('protector_validate_code') }}">
-    <BR>Code: <input type="text" name="code">
-    <div id="protector"></div>
-    <button type="button" id="reload" onclick="getProtectorFields()">Reload code</button>
-    <input type="submit" value="send">
-</form>
-
-</body>
-</html>
-
-
-<script>
-    function getProtectorFields() {
-        let url = '{{ route('protector_form') }}';
-        let recaptcha = document.getElementById('protector');
-        fetch(url)
-            .then(response => response.text())
-            .then(data => {
-                recaptcha.innerHTML = data;
-            })
-            .catch(error => {
-                console.error('Грешка:', error);
-            });
-    }
-    getProtectorFields();
-</script>
+@section('content')
+    <form method="post" action="{{ route('protector_validate_code') }}">
+        <BR>Code: <input type="text" name="code">
+        <div id="protector"></div>
+        <button type="button" id="reload" onclick="getProtectorFields()">Reload code</button>
+        <input type="submit" value="send">
+    </form>
+    <script>
+        (function () {
+            let reloadButton = document.getElementById('reload');
+            reloadButton.addEventListener("click", getProtectorFields);
+            function getProtectorFields() {
+                let url = '{{ route('protector_form') }}';
+                let recaptcha = document.getElementById('protector');
+                fetch(url)
+                    .then(response => response.text())
+                    .then(data => {
+                        recaptcha.innerHTML = data;
+                    })
+                    .catch(error => {
+                        console.error('Грешка:', error);
+                    });
+            }
+            getProtectorFields();
+        })();
+    </script>
+@endsection
