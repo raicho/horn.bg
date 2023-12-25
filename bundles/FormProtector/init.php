f050daeaa10f0968d7a90fd93c3356673fdf00ed
Райчо <raioosered@gmail.com>
2023-12-25 14:32:17 +0200

    Use Loader to load classes, config, routes

--- bundles/FormProtector/init.php
+++ bundles/FormProtector/init.php
@@ -7,39 +7,18 @@ const FORM_PROTECTOR_ROUTES_PATH = FORM_PROTECTOR_BASE_PATH.'Routes/';
 const FORM_PROTECTOR_MODELS_PATH = FORM_PROTECTOR_BASE_PATH.'Models/';
 
 use FormProtector\Services\FormProtectorServiceProvider;
-
+use Rkstylex\Services\Loader;
 
 // load config //
-
-require_once (FORM_PROTECTOR_CONFIG_PATH.'config.php');
-
-
+Loader::loadFilesInDirectory(FORM_PROTECTOR_CONFIG_PATH);
 // load service //
 require_once (FORM_PROTECTOR_SERVICES_PATH.'FormProtectorServiceProvider.php');
 // register service //
 app()->register(FormProtectorServiceProvider::class);
-
 // load all classes
-$classesDirectory = scandir(FORM_PROTECTOR_CLASSES_PATH);
-$classesDirectory = array_diff($classesDirectory, array('.', '..'));
-foreach($classesDirectory as $file) {
-    require_once (FORM_PROTECTOR_CLASSES_PATH.$file);
-}
-
-// load models
-$routesDirectory = scandir(FORM_PROTECTOR_MODELS_PATH);
-$routesDirectory = array_diff($routesDirectory, array('.', '..'));
-foreach($routesDirectory as $file) {
-    require_once (FORM_PROTECTOR_MODELS_PATH.$file);
-}
-
-
-// load routes //
-$routesDirectory = scandir(FORM_PROTECTOR_ROUTES_PATH);
-$routesDirectory = array_diff($routesDirectory, array('.', '..'));
-foreach($routesDirectory as $file) {
-    require_once (FORM_PROTECTOR_ROUTES_PATH.$file);
-}
-
-
+Loader::loadFilesInDirectory(FORM_PROTECTOR_CLASSES_PATH);
+// load all models
+Loader::loadFilesInDirectory(FORM_PROTECTOR_MODELS_PATH);
+// load all routes //
+Loader::loadFilesInDirectory(FORM_PROTECTOR_ROUTES_PATH);
 
