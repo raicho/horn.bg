8b7d58af9519b3020c00b8cf4330f3e4cbf512ec
Райчо <raioosered@gmail.com>
2023-12-25 13:01:15 +0200

    Jquery and bootstrap added

--- bundles/FormProtector/Resources/views/example.blade.php
+++ bundles/FormProtector/Resources/views/example.blade.php
@@ -1,12 +1,30 @@
 @extends('layouts.master')
 
 @section('content')
-    <form method="post" action="{{ route('protector_validate_code') }}">
-        <BR>Code: <input type="text" name="code">
+
+    <form method="POST" class="form-control mt-5" action="{{ route('protector_validate_code') }}">
+        <h3 class="text-center mt-3 mb-3">Example</h3>
+        <div class="mb-3 row">
+            <label for="code" class="col-sm-2 col-form-label">Code</label>
+            <div class="col-sm-10">
+                <input class="form-control" id="code" placeholder="Type code" type="text" name="code">
+            </div>
+        </div>
         <div id="protector"></div>
-        <button type="button" id="reload">Reload code</button>
-        <input type="submit" value="send">
+        <button type="button" class="btn btn-secondary mt-3" id="reload">New code</button>
+
+        <button type="submit" class="btn btn-primary mt-3 float-md-end">Check code</button>
     </form>
+
+
+    <style>
+        #protector img {
+            width: 100%;
+            border-radius: 5px;
+            border: 4px double #000;
+        }
+    </style>
+
     <script>
         (function () {
             let reloadButton = document.getElementById('reload');
@@ -14,6 +32,11 @@
             function getProtectorFields() {
                 let url = '{{ route('protector_form') }}';
                 let recaptcha = document.getElementById('protector');
+                reloadButton.disabled = true;
+                setTimeout(function() {
+                    reloadButton.disabled = false;
+                }, 1000);
+
                 fetch(url)
                     .then(response => response.text())
                     .then(data => {
