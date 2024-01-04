68c2af30237553147d1a8e3b78fe49be60289910
root <raioosered@gmail.com>
2024-01-04 13:31:56 +0200

    update database seeders

--- bundles/User/Seeders/UserSeeder.php
+++ bundles/User/Seeders/UserSeeder.php
@@ -9,7 +9,7 @@ class UserSeeder extends Seeder
 {
     public function run()
     {
-        for($i =0; $i<= 10; $i++) {
+        for($i =0; $i<= 1000; $i++) {
              User::factory(1)->create([
                  'is_admin' => rand(0,1),
                  'is_verified' => rand(0,1)
